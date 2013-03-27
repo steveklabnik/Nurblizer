bcf45dfe2ef505bf4fa3321d62313e7f0a301a6f
Steve Klabnik <steve@steveklabnik.com>
2013-03-27 12:24:43 -0700

    Use do/end for multi-line blocks

--- app.rb
+++ app.rb
@@ -6,13 +6,13 @@ def nurble(text)
   text = text.upcase
   words = text.downcase().gsub(/[^a-z ]/, '').split
 
-  words.each{|w|
+  words.each do |w|
     if not settings.nouns.include? w
       pattern = Regexp.new('(\b)'+ w + '(\b)', Regexp::IGNORECASE)
       replacement = "\1<span class=\"nurble\">nurble</span>\2"
       text.gsub! pattern, replacement
     end
-  }
+  end
   text.gsub(/\n/, '<br>')
 end
 
